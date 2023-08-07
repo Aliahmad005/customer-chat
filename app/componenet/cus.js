a5c4a335aa959352d1ae8f8c00c111d2f8d2a90b
Aliahmad005 <aliahmad051999@gmail.com>
2023-08-07 19:50:01 +0500

    main

---
+++ app/componenet/cus.js
@@ -0,0 +1,15 @@
+"use client"
+import React from 'react'
+import { FacebookProvider, CustomChat } from 'react-facebook';
+const Cus = () => {
+  return (
+    <div>
+      <p>customer chat</p>
+       <FacebookProvider appId="1611389066016659" chatSupport>
+        <CustomChat pageId="107380975202361" minimized={false}/>
+      </FacebookProvider>  
+    </div>
+  )
+}
+
+export default Cus
